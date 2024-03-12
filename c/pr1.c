0a005c6fadfe01055340c44767b484613ebb7ea0
bhaktiii11 <162980251+bhaktiii11@users.noreply.github.com>
2024-03-12 15:47:10 +0530

    Add files via upload

---
+++ c/pr1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+    float a,b,c;
+    printf("enter the price :");
+    scanf("%f",&a);
+    printf("enter the price :");
+    scanf("%f",&b);
+
+    c=(a*b)/100;
+
+    printf("The calculates the discount :%f",c);
+
+
+}
