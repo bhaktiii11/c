43d6578c3480d0c9457786621a848abcd84810f9
bhaktiii11 <162980251+bhaktiii11@users.noreply.github.com>
2024-04-24 16:27:49 +0530

    Add files via upload

---
+++ f-exam/7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int rows = 5;
+    int number = 10;
+    
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("%d ", number * number);
+        }
+        printf("\n");
+        number -= 1;
+    }
+    
+    return 0;
+}
