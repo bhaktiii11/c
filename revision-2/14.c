0f88efc6d11dbf1b99c3b61b4340d4842138ffc9
bhaktiii11 <162980251+bhaktiii11@users.noreply.github.com>
2024-05-02 15:56:32 +0530

    Add files via upload

---
+++ revision-2/14.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverseWords(char *str) {
+    char *start = str;
+
+    while (*str) {
+        if (*str == ' ' || *str == '\0') {
+            char *end = str - 1;
+            while (start < end) {
+                char temp = *start;
+                *start++ = *end;
+                *end-- = temp;
+            }
+            start = str + 1;
+        }
+        str++;
+    }
+    char *end = str - 1;
+    while (start < end) {
+        char temp = *start;
+        *start++ = *end;
+        *end-- = temp;
+    }
+}
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    str[strcspn(str, "\n")] = '\0';
+
+    reverseWords(str);
+
+    printf("Reversed string: %s\n", str);
+
+}
