c63889e0d5594bbae458cd7e6c53ae1796269359
bhaktiii11 <162980251+bhaktiii11@users.noreply.github.com>
2024-10-06 18:08:45 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import Crud2 from './Crud2/Crud2'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Crud & Routing/Home';
+import Create from './Crud & Routing/Create';
+import Delete from './Crud & Routing/Delete';
+import Update from './Crud & Routing/Update';
+import { useState } from 'react';
+
+
+
 
 export default function App() {
+  const [data, setData] = useState(
+    [
+      { id: 1, name: "sumit", subject: "react"},
+      { id: 2, name: "rahul", subject: "nodejs"},
+    ]
+  )
   return (
     <div>
-        <Crud2/>
-    </div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home data={data}/>}></Route>
+        <Route path='/create' element={<Create data={data} setData={setData}/>}></Route>
+        <Route path='/delete' element={<Delete data={data} setData={setData}/>}></Route>
+        <Route path='/update' element={<Update/>}></Route>
+        </Routes>
+        </BrowserRouter>
+      </div>
   )
 }
